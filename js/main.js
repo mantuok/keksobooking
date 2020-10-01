dd3c107c0ea9d9013802a52f3e997ec282c82110
mantuok <mantuok@gmail.com>
2020-10-01 16:23:56 +0300

    Исправления на основании комментариев

--- js/main.js
+++ js/main.js
@@ -1,86 +1,67 @@
 'use strict';
 
-const AVATAR_NUMBER = [1, 2, 3, 4, 5, 6, 7, 8];
-const LISTING_TYPE = [`palace`, `flat`, `house`, `bungalow`];
-const CHECKIN_CHECKOUT = [`12:00`, `13:00`, `14:00`];
-const PRICE = [1000, 2000, 3000, 4000, 5000];
-const ROOMS = [1, 2, 3, 4];
-const GUESTS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const PROPERTY_TYPE = [`palace`, `flat`, `house`, `bungalow`];
 const FEATURES = [`wifi`, `dishwasher`, `parking`, `washer`, `elevator`, `conditioner`];
+const CHECKIN_CHECKOUT = [`12:00`, `13:00`, `14:00`];
 const PHOTOS = [
   `http://o0.github.io/assets/images/tokyo/hotel1.jpg`,
   `http://o0.github.io/assets/images/tokyo/hotel2.jpg`,
   `http://o0.github.io/assets/images/tokyo/hotel3.jpg`
 ];
-const LISTING_NUMBER = 8;
+const ADVERT_NUMBER = 8;
 const pinTemplate = document.querySelector(`#pin`).content.querySelector(`.map__pin`);
 const pins = document.querySelector(`.map__pins`);
 const map = document.querySelector(`.map`);
 
-const getUniqueElement = (arr) => {
-  const uniqueElement = arr[0];
-  arr.splice(0, 1);
-  return uniqueElement;
+const getRandom = (min, max) => Math.floor(Math.random() * (max - min) + min);
+
+const getRandomFrom = (arr) => arr[getRandom(0, arr.length - 1)];
+
+const getRandomArray = (arr) => arr.slice(getRandom(0, arr.length / 2), getRandom(arr.length / 2 + 1, arr.length - 1));
+
+const generateAdvert = () => {
+  const location = {
+    x: getRandom(0, map.offsetWidth),
+    y: getRandom(130, 650)
+  };
+  return {
+    author: {
+      avatar: `img/avatars/user0${getRandom(1, 8)}.png`
+    },
+    offer: {
+      title: `Lorem ipsum`,
+      address: `${location.x}, ${location.y}`,
+      price: 1000,
+      type: getRandomFrom(PROPERTY_TYPE),
+      rooms: 2,
+      guests: 4,
+      checkin: getRandomFrom(CHECKIN_CHECKOUT),
+      checkout: getRandomFrom(CHECKIN_CHECKOUT),
+      feutures: getRandomArray(FEATURES),
+      description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere libero a velit hendrerit mattis.`,
+      photos: getRandomArray(PHOTOS),
+      location
+    }};
 };
 
-const getRandomElement = (min, max) => Math.floor(Math.random() * (max - min) + min);
+const generateAdverts = (amount) => new Array(amount).fill(``).map(generateAdvert);
 
-const getRandomArray = (arr) => {
-  const newArr = [];
-  const newArrLength = getRandomElement(1, arr.length - 1);
-  for (let i = 0; i < newArrLength; i++) {
-    newArr.push(arr[i]);
-  }
-  return newArr;
-};
-
-const createListing = () => ({
-  author: {
-    avatar: `img/avatars/user0${getUniqueElement(AVATAR_NUMBER)}.png`
-  },
-  offer: {
-    title: `Lorem ipsum`,
-    address: `500, 300`,
-    price: PRICE[getRandomElement(0, PRICE.length - 1)],
-    type: LISTING_TYPE[getRandomElement(0, LISTING_TYPE.length - 1)],
-    rooms: ROOMS[getRandomElement(0, ROOMS.length - 1)],
-    guests: GUESTS[getRandomElement(0, GUESTS.length - 1)],
-    checkin: CHECKIN_CHECKOUT[getRandomElement(0, CHECKIN_CHECKOUT.length - 1)],
-    checkout: CHECKIN_CHECKOUT[getRandomElement(0, CHECKIN_CHECKOUT.length - 1)],
-    feutures: getRandomArray(FEATURES),
-    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere libero a velit hendrerit mattis.`,
-    photos: getRandomArray(PHOTOS),
-    location: {
-      x: getRandomElement(0, map.offsetWidth),
-      y: getRandomElement(130, 650)
-    }
-  }
-});
-
-const createListingArray = (listingNumber) => {
-  const listings = [];
-  for (let i = 0; i < listingNumber; i++) {
-    listings.push(createListing());
-  }
-  return listings;
-};
-
-const renderPin = (listing) => {
+const renderPin = (advert) => {
   const pinElement = pinTemplate.cloneNode(true);
-  pinElement.style = `top: ${listing.offer.location.y + pinElement.offsetHeight / 2}px; left: ${listing.offer.location.x + pinElement.offsetWidth / 2}px`;
-  pinElement.querySelector(`img`).src = `${listing.author.avatar}`;
-  pinElement.querySelector(`img`).alt = `${listing.offer.title}`;
+  pinElement.style = `top: ${advert.offer.location.y + pinElement.offsetHeight / 2}px; left: ${advert.offer.location.x + pinElement.offsetWidth / 2}px`;
+  pinElement.querySelector(`img`).src = `${advert.author.avatar}`;
+  pinElement.querySelector(`img`).alt = `${advert.offer.title}`;
   return pinElement;
 };
 
-const renderPins = (listings) => {
+const renderPins = (adverts) => {
   const fragment = document.createDocumentFragment();
-  listings.map(renderPin).forEach((renderedPin) => fragment.appendChild(renderedPin));
+  adverts.map(renderPin).forEach((renderedPin) => fragment.appendChild(renderedPin));
   pins.appendChild(fragment);
 };
 
-const listings = createListingArray(LISTING_NUMBER);
+const adverts = generateAdverts(ADVERT_NUMBER);
 
-renderPins(listings);
+renderPins(adverts);
 
 map.classList.remove(`map--faded`);
